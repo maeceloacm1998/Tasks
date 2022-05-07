272401a126964f82ee6d8a5a4cb12a2fab6de92f
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-05-07 15:38:02 -0300

    feat: add Interception headers

--- app/src/main/java/com/example/tasks/service/repository/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/tasks/service/repository/remote/RetrofitClient.kt
@@ -1,20 +1,35 @@
 package com.example.tasks.service.repository.remote
 
+import com.example.tasks.service.constants.TaskConstants
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitClient private constructor() {
+
     companion object {
         private lateinit var retrofit: Retrofit
         private var BASE_URL = "http://devmasterteam.com/CursoAndroidAPI/"
+        private var personKey = ""
+        private var tokenKey = ""
 
         fun getRetrofitInstance(): Retrofit {
-            val httpCleint = OkHttpClient.Builder()
+            val httpClient = OkHttpClient.Builder()
+
+            httpClient.addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader(TaskConstants.HEADER.TOKEN_KEY, tokenKey)
+                    .addHeader(TaskConstants.HEADER.PERSON_KEY, personKey).build()
+
+                chain.proceed(request)
+            }
+
             if (!Companion::retrofit.isInitialized) {
                 retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .client(httpCleint.build())
+                    .client(httpClient.build())
                     .addConverterFactory(GsonConverterFactory.create()).build()
             }
             return retrofit
@@ -22,6 +37,11 @@ class RetrofitClient private constructor() {
 
         fun <T> createService(service: Class<T>): T {
             return getRetrofitInstance().create(service)
+        }
+
+        fun addHeader(token: String, person: String) {
+            tokenKey = token
+            personKey = person
         }
     }
 }
--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -10,6 +10,7 @@ import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.model.HeaderModel
 import com.example.tasks.service.repository.LoginRepository
 import com.example.tasks.service.repository.local.SecurityPreferences
+import com.example.tasks.service.repository.remote.RetrofitClient
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
     private var mSecurityPreferences = SecurityPreferences(application.applicationContext)
@@ -18,11 +19,8 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     private var mLogin = MutableLiveData<Boolean>().apply {
         value = false
     }
-    val login:LiveData<Boolean> = mLogin
+    val login: LiveData<Boolean> = mLogin
 
-    /**
-     * Faz login usando API
-     */
     fun doLogin(email: String, password: String) {
         mLoginRepository.login(email, password, object : APIListener<HeaderModel> {
             override fun onSuccess(model: HeaderModel) {
@@ -32,25 +30,29 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
                     store(TaskConstants.SHARED.PERSON_NAME, model.name)
                 }
 
+                applyHeaders(model.token, model.personKey)
+
                 mLogin.value = true
             }
 
             override fun onFailure(message: String) {
-                Toast.makeText(getApplication(),message,Toast.LENGTH_LONG).show()
+                Toast.makeText(getApplication(), message, Toast.LENGTH_LONG).show()
             }
         })
     }
 
-    /**
-     * Verifica se usuário está logado
-     */
     fun verifyLoggedUser() {
         val token = mSecurityPreferences.get(TaskConstants.SHARED.TOKEN_KEY)
         val personToken = mSecurityPreferences.get(TaskConstants.SHARED.PERSON_KEY)
 
-        if(token != ""  && personToken != ""){
+        applyHeaders(token, personToken)
+
+        if (token != "" && personToken != "") {
             mLogin.value = true
         }
     }
+
+    private fun applyHeaders(tokenKey: String, personKey: String) =
+        RetrofitClient.addHeader(tokenKey, personKey)
 
 }
